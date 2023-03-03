eaf80b4d380a416439699d1d1abb8db89fa40e5b
nayeong <ny567@naver.com>
2023-03-03 18:13:26 +0900

    230303

--- Java_KNY/src/oop/basic/MemberLocalVariable.java
+++ Java_KNY/src/oop/basic/MemberLocalVariable.java
@@ -18,8 +18,20 @@ package oop.basic;
 
 public class MemberLocalVariable {
 
+	int a; //멤버 변수(필드)
+	
+	void printNumber(int c) { //매개 변수(일종의 지역 변수)
+		int b = 1; //지역 변수
+		System.out.println("멤버변수 a: " + a);
+		System.out.println("지역변수 b: " + b);
+		System.out.println("매개변수 c: " + c);
+		
+	}
+	
 	public static void main(String[] args) {
 
+		MemberLocalVariable m = new MemberLocalVariable();
+		m.printNumber(4);
 	}
 
 }
